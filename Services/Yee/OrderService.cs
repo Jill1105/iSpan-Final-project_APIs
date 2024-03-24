9914971cfdbe5d99f6f763281e473cac72b79049
fuen31-wf <fuen31wf@gmail.com>
2024-03-24 18:23:13 +0800

    feat: 訂單-新增訂單測試

--- Services/Yee/OrderService.cs
+++ Services/Yee/OrderService.cs
@@ -3,6 +3,7 @@ using HotelFuen31.APIs.Models;
 using Microsoft.AspNetCore.Routing.Matching;
 using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
+using System.Transactions;
 using static NuGet.Packaging.PackagingConstants;
 
 namespace HotelFuen31.APIs.Services.Yee
@@ -21,49 +22,61 @@ namespace HotelFuen31.APIs.Services.Yee
         public int CreateOrder(string phone)
         {
             var selectedItem = _db.CartRoomItems.Where(cri => cri.Phone == phone && cri.Selected);
-            if (selectedItem.Any()) throw new Exception("訂單項目為空");
+            if (!selectedItem.Any()) throw new Exception("訂單項目為空");
 
-            // 建立訂單
-            var order = new Order
+
+            using (var scope = new TransactionScope())
             {
-                Phone = phone,
-                Status = 0,
-                OrderTime = DateTime.Now,
-            };
-            _db.Orders.Add(order);
-
-            // 逐一寫入
-            selectedItem.ToList().ForEach(item =>
-            {
-                // 確認有無空房
-                var room = _db.Rooms
-                               .Include(rt => rt.RoomBookings)
-                               .FirstOrDefault(r => !r.RoomBookings.Any(rb => (item.CheckInDate < rb.CheckOutDate && item.CheckOutDate > rb.CheckInDate)));
-
-                if (room == null) throw new Exception("已無空房");
-
-                // 新建訂單項目
-                var newBooking = new RoomBooking
+                // 建立訂單
+                var order = new Order
                 {
-                    RoomId = room.RoomId,
-                    CheckInDate = item.CheckInDate,
-                    CheckOutDate = item.CheckOutDate,
-                    MemberId = _db.Members.FirstOrDefault(m => m.Phone == phone)?.Id ?? null,
-                    Remark = item.Remark,
-                    BookingStatusId = 2,
-                    BookingDate = order.OrderTime,
-                    OrderPirce = _crService.GetPrice(item.CheckInDate, item.CheckOutDate, item.TypeId),
                     Phone = phone,
+                    Status = 0,
+                    OrderTime = DateTime.Now,
                 };
+                _db.Orders.Add(order);
+                _db.SaveChanges();
 
-                _db.RoomBookings.Add(newBooking);
-            });
+                // 逐一寫入
+                selectedItem.ToList().ForEach(item =>
+                {
+                    // 確認有無空房
+                    var room = _db.Rooms
+                                   .Include(r => r.RoomBookings)
+                                   .Where(r => r.RoomTypeId == item.TypeId)
+                                   .FirstOrDefault(r => !r.RoomBookings.Any(rb => (item.CheckInDate < rb.CheckOutDate && item.CheckOutDate > rb.CheckInDate)));
 
-            // 購物車排除
-            _db.CartRoomItems.RemoveRange(selectedItem);
-            _db.SaveChanges();
+                    if (room == null) throw new Exception("已無空房");
 
-            return order.Id;
+                    // 新建訂單項目
+                    var newBooking = new RoomBooking
+                    {
+                        RoomId = room.RoomId,
+                        OrderId = order.Id,
+                        CheckInDate = item.CheckInDate,
+                        CheckOutDate = item.CheckOutDate,
+                        MemberId = _db.Members.FirstOrDefault(m => m.Phone == phone)?.Id ?? null,
+                        Remark = item.Remark,
+                        BookingStatusId = 2,
+                        BookingDate = order.OrderTime,
+                        OrderPirce = _crService.GetPrice(item.CheckInDate, item.CheckOutDate, item.TypeId),
+                        Phone = phone,
+                    };
+
+                    _db.RoomBookings.Add(newBooking);
+                    _db.SaveChanges();
+                });
+
+                // 購物車排除已提交訂單
+                _db.CartRoomItems.RemoveRange(selectedItem);
+                _db.SaveChanges();
+
+                // 提交事务
+                scope.Complete();
+
+                return order.Id;
+            }
+            
         }
     }
 }
