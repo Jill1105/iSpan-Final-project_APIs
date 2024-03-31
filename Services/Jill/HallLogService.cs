4dfd910f0688a8b94fde148c1a843b08abc8cbed
Jill <1105105104@nkust.edu.tw>
2024-03-31 23:59:32 +0800

    feat 新增紀錄查詢

---
+++ Services/Jill/HallLogService.cs
@@ -0,0 +1,63 @@
+﻿using HotelFuen31.APIs.Dtos.Jill;
+using HotelFuen31.APIs.Models;
+using Humanizer;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelFuen31.APIs.Services.Jill
+{
+    public class HallLogService
+    {
+        private readonly AppDbContext _context;
+        public HallLogService(AppDbContext context)
+        { 
+            _context = context; 
+        }
+
+        public IQueryable<HallLogDto> GetAll() {
+
+            var dto = _context.HallLogs
+                .AsNoTracking()
+                .Select(h => new HallLogDto
+                {
+                    Id = h.Id,
+                    HallId = h.HallId,
+                    UserId = h.UserId,
+                    Guests = h.Guests,
+                    StartTime = h.StartTime,
+                    EndTime = h.EndTime,
+                    BookingStatus = h.BookingStatus,
+                    Name = h.Name,
+                    CellPhone = h.CellPhone,
+                    Email = h.Email,
+                    FilePath = h.FilePath,
+                    HallName = h.Hall.HallName,
+                });
+
+            return dto;
+        }
+
+        public IQueryable<HallLogDto> SearchLog(int id) 
+        {
+            var query = _context.HallLogs
+                .AsNoTracking()
+                .Where(h => h.Hall.Id == id)
+                .Select(h => new HallLogDto
+                {
+                    Id = h.Id,
+                    HallId = h.HallId,
+                    UserId = h.UserId,
+                    Guests = h.Guests,
+                    StartTime = h.StartTime,
+                    EndTime = h.EndTime,
+                    BookingStatus = h.BookingStatus,
+                    Name = h.Name,
+                    CellPhone = h.CellPhone,
+                    Email = h.Email,
+                    FilePath = h.FilePath,
+                    HallName = h.Hall.HallName,
+                });
+
+            return query;
+        }
+    }
+}
